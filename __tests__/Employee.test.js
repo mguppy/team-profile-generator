f18c156d7306defab19f9f031f5a4ee3a15afacb
mguppy <mguppy34@gmail.com>
2021-07-19 15:40:14 -0500

    finished employee class tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -31,7 +31,27 @@ test('email argument is passed successfully', ()=>{
 
 //Test the methods
 //getName
+test('getName method works correctly', ()=>{
+    const name = 'Maryam';
+    const emp = new Employee(name);
+    expect(emp.getName()).toBe(name);
+});
 //getID
+test('ID argument is passed successfully', ()=>{
+    const id = 12345;
+    const emp = new Employee('Maryam',id);
+    expect(emp.getID()).toBe(id);
+});
 //getEmail
+test('email argument is passed successfully', ()=>{
+    const email = 'mguppy34@gmail.com';
+    const emp = new Employee('Maryam',12345,email);
+    expect(emp.getEmail()).toBe(email);
+});
 //getRole
+test('role argument is passed successfully', ()=>{
+    const role = 'Employee';
+    const emp = new Employee('Maryam',12345,'mguppy34@gmail.com');
+    expect(emp.getRole()).toBe(role);
+});
 
