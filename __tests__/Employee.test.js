67f04fffe66a4597f057df1958421800b0e49f67
mguppy <mguppy34@gmail.com>
2021-07-19 15:34:44 -0500

    arguments written, tested and passed

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,37 @@
+const { TestWatcher } = require('jest');
+const Employee = require('../lib/Employee');
+
+//Does it return an object?
+test('class employee returns an object', ()=> {
+    const emp = new Employee();
+    expect(typeof(emp)).toBe('object');
+});
+
+//Test the arguments
+//Name argument 
+test('name argument is passed successfully', ()=>{
+    const name = 'Maryam';
+    const emp = new Employee(name);
+    expect(emp.name).toBe(name);
+});
+
+//ID argument
+test('ID argument is passed successfully', ()=>{
+    const id = 12345;
+    const emp = new Employee('Maryam',id);
+    expect(emp.id).toBe(id);
+});
+
+//Email argument
+test('email argument is passed successfully', ()=>{
+    const email = 'mguppy34@gmail.com';
+    const emp = new Employee('Maryam',12345,email);
+    expect(emp.email).toBe(email);
+});
+
+//Test the methods
+//getName
+//getID
+//getEmail
+//getRole
+
